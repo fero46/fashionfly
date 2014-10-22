6e2b5a832e16962ae9f0bc4017df9ca61f806602
Ferhat Ziba <fero.ziba@gmail.com>
2014-10-22 02:04:05 +0200

    css styling start

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def like_url
+    require 'uri'
+    URI.escape("http://crocodealia.de/")
+  end
+
+  def scope
+    (@scope ||= Scope.where(locale: locale).first) if params[:locale]
+  end
 end
