7eb6394629de23c7d2048f17cf2fcea25261a0af
Ferhat Ziba <fero.ziba@gmail.com>
2014-11-13 18:43:16 +0100

    category path now with full url

--- app/helpers/api/catgegories_helper.rb
+++ app/helpers/api/catgegories_helper.rb
@@ -2,6 +2,6 @@ module Api::CatgegoriesHelper
   def category_icon category
     icons = category.icons.first
     result = icons.try(:image).try(:url)
-    result ? result : ""
+    result ? request.protocol + request.host_with_port + result : ""
   end
 end
