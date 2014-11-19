938fa457b0e1bf34cd72e2525fff653d7c8e419c
Ferhat Ziba <fero.ziba@gmail.com>
2014-11-19 17:34:16 +0100

    add editable static sites

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,28 @@
 class StaticController < ScopeController
   def faq
+    render 'static', locals: {name: '__faq__page__'}
+  end
+
+  def privacy
+    render 'static', locals: {name: '__faq__page__'}
+  end
+
+  def disclaimer
+    render 'static', locals: {name: '__faq__page__'}
+  end
+
+  def impress
+    render 'static', locals: {name: '__impress__page__'}
   end
 
   def about
+    render 'static', locals: {name: '__about__page__'}
+  end
+
+  def terms
+    render 'static', locals: {name: '__terms__page__'}
+  end
+
+  def team
   end
 end
--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,13 @@
 module StaticHelper
+  def render_static_scope_page name
+    if @scope.page(name).present?
+      content_tag 'div', class: 'container whitebox' do 
+        concat content_tag('h1', @scope.page(name).title) if @scope.page(name).title.present?
+        concat simple_format @scope.page(name).body
+      end
+    else
+      content_tag 'div', '',id: name, style: 'display:none'
+    end
+  end
+
 end
