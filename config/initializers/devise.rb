4ae4015214bc085060fce4ff981398c593d58e7d
Ferhat Ziba <fero.ziba@gmail.com>
2014-12-15 00:13:07 +0100

    add oath

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -196,10 +196,10 @@ Devise.setup do |config|
 
 
    config.omniauth :facebook, Settings.oauth.facebook.id, Settings.oauth.facebook.secret
-   config.omniauth :twitter, 'APP_ID', 'APP_SECRET'
-   config.omniauth :pinterest, 'APP_ID', 'APP_SECRET'
-   config.omniauth :tumblr, 'APP_ID', 'APP_SECRET'
-   config.omniauth :google, 'APP_ID', 'APP_SECRET'
+   config.omniauth :twitter, Settings.oauth.twitter.id, Settings.oauth.twitter.secret
+   # config.omniauth :pinterest, 'APP_ID', 'APP_SECRET'
+   config.omniauth :tumblr, Settings.oauth.tumblr.id, Settings.oauth.tumblr.secret
+   config.omniauth :google, Settings.oauth.google.id, Settings.oauth.google.secret
 
 
   # ==> Warden configuration
