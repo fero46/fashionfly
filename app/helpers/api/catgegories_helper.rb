fcd5454e213ec7d8b2bb60f6e9c8ceccdd117ac1
Ferhat Ziba <fero.ziba@gmail.com>
2014-12-15 16:04:11 +0100

    fix icon path

--- app/helpers/api/catgegories_helper.rb
+++ app/helpers/api/catgegories_helper.rb
@@ -2,6 +2,10 @@ module Api::CatgegoriesHelper
   def category_icon category
     icons = category.icons.first
     result = icons.try(:image).try(:url)
-    result ? request.protocol + request.host_with_port + result : ""
+    if result.present? && (result.start_with?("http://") || result.start_with?("https://"))
+      request.protocol + request.host_with_port + result
+    else
+      result
+    end
   end
 end
