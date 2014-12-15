2a0114c69b647183f0cda02d6e6280fd862c6691
Ferhat Ziba <fero.ziba@gmail.com>
2014-12-15 07:31:03 +0100

    fix clockwork path

--- clock.rb
+++ clock.rb
@@ -1,9 +1,7 @@
-app_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-$LOAD_PATH.unshift(app_path) unless $LOAD_PATH.include?(app_path)
-
+app_path = File.expand_path(File.join(File.dirname(__FILE__)))
 require 'clockwork'
-require './config/boot'
-require './config/environment'
+require "#{app_path}/config/boot"
+require "#{app_path}/config/environment"
 
 module Clockwork
   handler do |job|
