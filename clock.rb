466ae21c30d8d55ae5eac9eaf61244cfb4be87fd
Ferhat Ziba <fero.ziba@gmail.com>
2014-12-15 07:26:53 +0100

    fix loadpath

--- clock.rb
+++ clock.rb
@@ -1,5 +1,7 @@
-require 'clockwork'
+app_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+$LOAD_PATH.unshift(app_path) unless $LOAD_PATH.include?(app_path)
 
+require 'clockwork'
 require './config/boot'
 require './config/environment'
 
