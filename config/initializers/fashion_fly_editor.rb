19351e353d301327e9865a814c8763398d673378
Ferhat Ziba <fero.ziba@gmail.com>
2014-12-16 15:10:38 +0100

    start customizing editor

--- config/initializers/fashion_fly_editor.rb
+++ config/initializers/fashion_fly_editor.rb
@@ -2,4 +2,5 @@ FashionFlyEditor::Engine.configure do |config|
   config.categories_endpoint = "/de/api/categories"
   config.startup_category_id = 487
   config.callbacks << :assign_collection
+  config.current_user = :current_user
 end
