e2d542db1dc113f2678712711b08f4f51d2f6cfd
Ferhat Ziba <fero.ziba@gmail.com>
2014-12-19 14:27:53 +0100

    fix wrong url in mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Fashionfly::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => "staging.fashionfly.de" }
+  config.action_mailer.default_url_options = { :host => "staging.fashionfly.co" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "localhost",
