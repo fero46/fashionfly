c7a40b9e94126f052a4432b15b3f256c8e44b50d
Ferhat Ziba <fero.ziba@gmail.com>
2015-01-07 19:07:03 +0100

    random_product_order fix, more simpler.

--- app/worker/random_product_order_worker.rb
+++ app/worker/random_product_order_worker.rb
@@ -2,25 +2,14 @@ class RandomProductOrderWorker
   include Sidekiq::Worker
 
   def perform
-    max_count = Product.count
-    scopes = Scope.where(published: true)
-    for scope in scopes
-      categories = scope.categories
-      for category in categories
-        max_count = category_product(category).count
-        category_product(category).find_in_batches(batch_size: 500) do |group|
-          group.each { |product| product.random_order = rand(max_count); product.save }
-        end
-      end
+    max_count = Product.where(published: true).count
+    Product.where(published: true).find_in_batches(batch_size: 500) do |group|
+      group.each { |product| product.random_order = rand(max_count); product.save }
     end
   end
-
-  def category_product category
-    category.products.where(published: true)
-  end
-
 
   def self.run
     perform_async()
   end
+  
 end
