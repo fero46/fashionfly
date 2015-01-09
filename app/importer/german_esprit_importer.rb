1c18f0d6249bbb96424c1832b7d84ab592ec86f0
Ferhat Ziba <fero.ziba@gmail.com>
2015-01-09 01:56:44 +0100

    fix largeimage

--- app/importer/german_esprit_importer.rb
+++ app/importer/german_esprit_importer.rb
@@ -4,16 +4,8 @@ class GermanEspritImporter < GenericImporter
 
 protected 
   
-  def product_remote_image values
-    remote_image = values['largeImage']
-    if remote_image.present?
-      bigger = remote_image.gsub('PicPOV_FlatView', 'PicSPV_ZoomFlat')
-      url = URI.parse(bigger)
-      req = Net::HTTP.new(url.host, url.port)
-      res = req.request_head(url.path)
-      remote_image = bigger if res.code == "200"
-    end
-    return remote_image
+  def product_remote_image(values)
+    values['largeImage']
   end
 
 end
