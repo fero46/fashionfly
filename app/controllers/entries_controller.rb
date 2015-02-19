97e8ac41ad3e6e2e9b7875ef111341a8dcd289cc
Ferhat Ziba <fero.ziba@gmail.com>
2015-02-19 16:08:09 +0100

    Add Blogger Link

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,9 @@
 class EntriesController < ApplicationController
 
+  def index
+    @entries = @scope.entries.page(params[:page]).per(params[:per].present? ? params[:per] : 20)
+  end
+
   def show
     @entry = @scope.entries.find(params[:id])
   end
