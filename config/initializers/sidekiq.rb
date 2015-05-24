320e27ef6b8448a83b24991a012d9492d9f68385
Ferhat Ziba <fero.ziba@gmail.com>
2015-05-24 10:24:47 +0200

    fix namespacing

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 redis_conn = proc {
-  Redis.new
+  Redis::Namespace.new(:fashionfly, :redis => Redis.new)
 }
 Sidekiq.configure_client do |config|
   config.redis = ConnectionPool.new(size: 5, &redis_conn)
