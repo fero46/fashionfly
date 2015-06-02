e21cbfe7ba163d90da95afc2a5a7466cc6b1ffab
Ferhat Ziba <fero.ziba@gmail.com>
2015-06-02 18:54:14 +0200

    fixed some details

--- app/importer/extended_reklam_action_importer.rb
+++ app/importer/extended_reklam_action_importer.rb
@@ -4,10 +4,20 @@ class ExtendedReklamActionImporter < ReklamActionImporter
   def find_category node
     sexes = nil
     cat = ""
+    switch_to_detail = false
     for subnode in node.children
       if subnode.name ==  MAIN_CAT || subnode.name == SUBCAT
         cat = "#{cat} - " if cat != ""
         cat = "#{cat}#{subnode.content}"
+        switch_to_detail = true
+      end
+      if subnode.name == "Categories"
+        for catigo_node in subnode.children
+          cat = "" if switch_to_detail
+          cat = "#{cat} - " if cat != ""
+          cat = "#{cat}#{catigo_node.content}"
+          switch_to_detail = false
+        end
       end
       if subnode.name == FullDesc
         content = subnode.content
@@ -25,12 +35,6 @@ class ExtendedReklamActionImporter < ReklamActionImporter
           sexes = "ERKEK"
         elsif content == "F" || content == "f"
           sexes = "BAYAN"
-        end
-      end
-      if subnode.name == "Categories"
-        for catigo_node in subnode.children
-          cat = "#{cat} - " if cat != ""
-          cat = "#{cat}#{catigo_node.content}"
         end
       end
     end
