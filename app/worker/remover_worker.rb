aad521c9777ccfb3240787469db6f9139e247e1c
Ferhat Ziba <fero.ziba@gmail.com>
2016-12-05 10:27:57 +0100

    add removerworker

---
+++ app/worker/remover_worker.rb
@@ -0,0 +1,16 @@
+class RemoverWorker
+  include Sidekiq::Worker
+
+  def perform(product_id)
+    product = Product.where(id: product_id).first
+    if product.present?
+      product.destroy
+    end
+  end
+
+  def self.run_importer product
+    return if product.blank?
+    perform_async(product.id)
+  end
+
+end
