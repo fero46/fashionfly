c4fa9edb29664b0d6006bf5943180680ec20294a
Ferhat Ziba <fero.ziba@gmail.com>
2017-10-07 23:59:35 +0200

    fix cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,12 @@ lock '3.5.0'
 set :application, 'fashionfly'
 set :repo_url, 'git@bitbucket.org:fashionfly/fashionfly.git'
 
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/sitemaps')
+
+
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -14,65 +20,16 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before 'puma:start', 'puma:make_dirs'
-end
-
 namespace :deploy do
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
   after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  before :restart, :symlink_upload do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :mkdir, "-p #{shared_path}/sitemaps"
-      execute :rm, "-rf","#{release_path}/public/sitemaps"
-      execute :ln, "-s", "#{shared_path}/sitemaps #{release_path}/public/sitemaps"
-      execute :rm, "-rf","#{release_path}/public/uploads"
-      execute :ln, "-s", "#{deploy_to}/shared/uploads #{release_path}/public/uploads"
-      execute :rm, "-rf","#{release_path}/log"
-      execute :ln, "-s", "#{deploy_to}/shared/log #{release_path}/log"
-      execute :rm, "-rf","#{release_path}/tmp"
-      execute :ln, "-s", "#{deploy_to}/shared/tmp #{release_path}/tmp"
-    end
-  end
-
-  before :compile_assets, :symlink_database do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :ln, "-nfs", "#{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-    end
-  end
-
 
 end
